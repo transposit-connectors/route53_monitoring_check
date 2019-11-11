f9475861359f1cb59b1e1e8bc5169676a16da4cb
Dan Moore <github@mooreds.com>
2019-11-11 14:29:46 -0700

    beautified

--- src/scheduled_job.js
+++ src/scheduled_job.js
@@ -1,15 +1,14 @@
 (params) => {
   const hostnames = api.run("this.list_hosted_zones").map(h => {
-    return h.Name.replace(/\.$/,"");
+    return h.Name.replace(/\.$/, "");
   });
   const uniq_hostnames = [...new Set(hostnames)];
-  
+
   const monitors = api.run("this.get_monitors")[0].monitor.map(m => {
-    return m.url.replace("https://","").replace("http://","").replace("/","");
+    return m.url.replace("https://", "").replace("http://", "").replace("/", "");
   });
-  
+
   const uniq_monitored_hostnames = [...new Set(monitors)];
-  const hostnames_missing_monitoring = uniq_hostnames.filter(x => !uniq_monitored_hostnames.includes(x) );
+  const hostnames_missing_monitoring = uniq_hostnames.filter(x => !uniq_monitored_hostnames.includes(x));
   return hostnames_missing_monitoring;
 }
-
--- src/untitled.js
+++ src/untitled.js
@@ -1,6 +1,8 @@
 (params) => {
   const unmonitored_hosts = api.run("this.unmonitored_hosts");
   unmonitored_hosts.forEach(host => {
-    api.run("this.post_chat_message",{text: "Warning: "+ host +" has been added to Route53 but is not monitored in UptimeRobot."})
+    api.run("this.post_chat_message", {
+      text: "Warning: " + host + " has been added to Route53 but is not monitored in UptimeRobot."
+    })
   })
 }
