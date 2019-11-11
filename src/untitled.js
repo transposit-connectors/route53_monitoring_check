12437d9084f3f3e4ec039ec64b53a1b62ff1b41c
Dan Moore <mooreds@transposit.com>
2019-11-11 21:18:05 +0000

    post message to slack

--- src/untitled.js
+++ src/untitled.js
@@ -1,6 +1,6 @@
 (params) => {
   const unmonitored_hosts = api.run("this.unmonitored_hosts");
-  if (unmonitored_hosts.length > 0) {
-    
-  }
+  unmonitored_hosts.forEach(host => {
+    api.run("this.post_chat_message",{text: "Warning: "+ host +" has been added to Route53 but is not monitored in UptimeRobot."})
+  })
 }
